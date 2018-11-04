49d96f99491cb94638797293a4421cf8b8bfe514
dong-shuai <599054912@qq.com>
2018-11-04 19:00:32 +0800

    最大范围的爬取深圳市的数据

--- scrawl.py
+++ scrawl.py
@@ -10,9 +10,22 @@ def get_communitylist():
 
 if __name__=="__main__":
     regionlist = settings.REGIONLIST # only pinyin support
-    model.database_init()
-    core.GetHouseByRegionlist(regionlist)
-    core.GetRentByRegionlist(regionlist)
-    core.GetCommunityByRegionlist(regionlist) # Init,scrapy celllist and insert database; could run only 1st time
-    communitylist = get_communitylist() # Read celllist from database
+    model.database_init() # only run on the first time
+    # ByRegionlist cant not get all data because linajie only display 100 pages
+    # core.GetHouseByRegionlist(regionlist)
+    # core.GetRentByRegionlist(regionlist)
+    
+    # Init,scrapy celllist and insert database; could run only 1st time
+    core.GetCommunityByRegionlist(regionlist) 
+    
+    # Read celllist from database
+    communitylist = get_communitylist()
+    
+    # history sell
     core.GetSellByCommunitylist(communitylist)
+    
+    # on sell
+    core.GetHouseByCommunitylist(communitylist)
+
+    # Rent 
+    core.GetRentByCommunitylist(communitylist)
--- settings.py
+++ settings.py
@@ -1,8 +1,8 @@
 DBENGINE = 'mysql' #ENGINE OPTIONS: mysql, sqlite3, postgresql
-DBNAME = 'test'
+DBNAME = 'lianjia_scrawler'
 DBUSER = 'root'
 DBPASSWORD = '123456'
 DBHOST = '127.0.0.1'
 DBPORT = 3306
-CITY = 'sz' # only one, shanghai=sh shenzhen=sh......
-REGIONLIST = [u'luohu'] # only pinyin support
+CITY = 'sz' # only one, shanghai=sh shenzhen=sz......
+REGIONLIST = [u'longgangqu', u'luohuqu', u'futianqu', u'yantianqu', u'longhuaqu', u'baoanqu', u'nanshanqu', u'guangmingxinqu' ,u'pingshanqu' ,u'dapengxinqu'] # only pinyin support
